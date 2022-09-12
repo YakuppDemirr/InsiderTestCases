51edb25cd43ffd685826178d7e4a62fb6318f606
YakuppDemirr <46319524+YakuppDemirr@users.noreply.github.com>
2022-09-12 14:31:45 +0300

    update

--- InsiderTestCaseApi/src/main/java/com/insider/base/BasePage.java
+++ InsiderTestCaseApi/src/main/java/com/insider/base/BasePage.java
@@ -5,5 +5,5 @@ import org.apache.log4j.Logger;
 public class BasePage {
 
     public static final Logger logger = Logger.getLogger("InsiderApi");
-    public final static String baseUrl = "https://petstore.swagger.io/v2/pet/";
+    public static final String baseUrl = "https://petstore.swagger.io/v2/pet/";
 }
